2adae165e85e282506869cbd14d07728cd9b8cb4
Ie Howe <iehowe@growthops.asia>
2020-04-14 17:59:58 +0800

    Added animations

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
+import ScrollOut from 'scroll-out';
 
 $(function () {
     // $('#milestone-modal').modal('show')
+
+    $('.animated.text-slide-in').each(function () {
+        // const currClientRect = this.getBoundingClientRect();
+        // const currLineHeight = parseInt($(this).css('line-height'));
+        // const numLines = currClientRect.height / currLineHeight;
+
+        $(this).html($(this).text().replace(/\S+/g, '<n>$&</n>'))
+        $(this).find('n').attr('data-scroll', true)
+    })
+
+    ScrollOut()
 })
